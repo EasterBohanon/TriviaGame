0d9feaf8b6ba36d3511492894e9891a26efced6d
Easter Bohanon <eastdabeast@email.arizona.edu>
2018-12-07 23:51:57 -0700

    I'm for sure working on it

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    var Myquestions = [
+    {
+        question: "Complete this sentence:",
+        answers:{
+            a: "",
+            b:"",
+            c:"",
+            d:"",
+        }
+
+    }
+]
+});
