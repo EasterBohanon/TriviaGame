5f242b38dc05d4bf096950020196adb5d0a25a94
Easter Bohanon <eastdabeast@email.arizona.edu>
2019-03-19 14:25:47 -0700

    getting much closer for sure B level

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,37 +5,37 @@
 var myQuestions = [
     {
         question: "What is Gus' Super Power?",
-        answers: [ "His sweet jazzy voice", "Financial stability","Super Sniffer", "Pyschic"],
+        answers: ["His sweet jazzy voice", "Financial stability", "Super Sniffer", "Pyschic"],
         correct: 'Super Sniffer',
         images: "assets/images/super_sniffer.gif"
     },
     {
         question: "What does Gus do for a living?",
-        answers: ["Professional Tap dancer", "Pluto Activist","lead psychic detective in a hit show","Pharmaceutical Rep"],
+        answers: ["Professional Tap dancer", "Pluto Activist", "lead psychic detective in a hit show", "Pharmaceutical Rep"],
         correct: 'Pharmaceutical Rep',
         images: "assets/images/jazzHands.gif"
     },
     {
+        question: "What is the fruit that is feautured in very episode?",
+        answers: ["Melon", "Banana", "Guava", "Pineapple"],
+        correct: 'Pineapple',
+        images: "assets/images/Shawn_Pineapple.gif"
+    },
+    {
         question: "What does Gus do for a living?",
-        answers: ["Professional Tap dancer", "Pluto Activist","lead psychic detective in a hit show","Pharmaceutical Rep"],
+        answers: ["Professional Tap dancer", "Pluto Activist", "lead psychic detective in a hit show", "Pharmaceutical Rep"],
         correct: 'Pharmaceutical Rep',
         images: "assets/images/super_sniffer.gif"
     },
     {
         question: "What does Gus do for a living?",
-        answers: ["Professional Tap dancer", "Pluto Activist","lead psychic detective in a hit show","Pharmaceutical Rep"],
+        answers: ["Professional Tap dancer", "Pluto Activist", "lead psychic detective in a hit show", "Pharmaceutical Rep"],
         correct: 'Pharmaceutical Rep',
         images: "assets/images/super_sniffer.gif"
     },
     {
         question: "What does Gus do for a living?",
-        answers: ["Professional Tap dancer", "Pluto Activist","lead psychic detective in a hit show","Pharmaceutical Rep"],
-        correct: 'Pharmaceutical Rep',
-        images: "assets/images/super_sniffer.gif"
-    },
-    {
-        question: "What does Gus do for a living?",
-        answers: ["Professional Tap dancer", "Pluto Activist","lead psychic detective in a hit show","Pharmaceutical Rep"],
+        answers: ["Professional Tap dancer", "Pluto Activist", "lead psychic detective in a hit show", "Pharmaceutical Rep"],
         correct: 'Pharmaceutical Rep',
         images: "assets/images/super_sniffer.gif"
     }
@@ -55,7 +55,7 @@ var game = {
     correct: 0,
     incorrect: 0,
 
-    countDown : function (){
+    countDown: function () {
         game.counter--;
         $("#time-remaining").append(game.counter);
         if (game.counter === 0) {
@@ -63,104 +63,115 @@ var game = {
             game.timeUp();
         }
     },
-    loadQuestion: function() {
-        timer = setInterval(game.countdown,1000);
+    loadQuestion: function () {
+        timer = setInterval(game.countdown, 1000);
         card.html("<h2>" + myQuestions[this.currentQuestion].question + "</h2>");
         console.log(myQuestions[this.currentQuestion].answers);
         for (var i = 0; i < myQuestions[this.currentQuestion].answers.length; i++) {
-            card.append("<br><button class='btn btn-primary btn-lg btn-block' type='button' id='answer-button' data-name='" + myQuestions[this.currentQuestion].answers[i] + "'>" + myQuestions[this.currentQuestion].answers[i]+ "</button><br>");
+            card.append("<br><button class='btn btn-primary btn-lg btn-block' type='button' id='answer-button' data-name='" + myQuestions[this.currentQuestion].answers[i] + "'>" + myQuestions[this.currentQuestion].answers[i] + "</button><br>");
 
         }
 
     },
-    nextQuestion: function(){
+    nextQuestion: function () {
         $("#time-remaining").html(game.counter);
         game.currentQuestion++;
         game.loadQuestion();
     },
 
-    timeUp: function(){
+    timeUp: function () {
         clearInterval(timer);
         $("#time-remaining").html(game.counter);
         card.html("<h2> Your Time Here Has Ended! </h2>");
         card.append("<h3> What You Wanted To Put Was:" + myQuestions[this.currentQuestion].correct);
-        card.append("<img src='" + myQuestions[this.currentQuestion].images +"' />" );
-        
-        if(game.currentQuestion === myQuestions.length - 1){
+        card.append("<img src='" + myQuestions[this.currentQuestion].images + "' />");
+
+        if (game.currentQuestion === myQuestions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         }
-        else{
+        else {
             setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
 
-    results: function() {
+    results: function () {
         clearInterval(timer);
-        card.html("<h2> All Done, Here is the true proof of your PSYCH-O-NESS </h2>");
-
         card.append(game.counter);
-        card.append(game.correct);
-       card.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
-       card.append("<h3>Unanswered: " + (myQuestions.length - (game.incorrect + game.correct)) + "</h3>");
+        card.html("<h2> All Done, Here is the true proof of your PSYCH-O-NESS </h2>");
+        card.append("<h3>Correct Answers: " + game.correct + "</h3>");
+        card.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+        card.append("<h3>Unanswered: " + (myQuestions.length - (game.incorrect + game.correct)) + "</h3>");
         card.append("<br><button id='start-over'>Start Over?</button>");
+        if (game.correct > 5) {
+            card.append("<h3>Jazz Hands For You mY True Psych-O!!!</h3>");
+            card.append("<br><img src='assets/images/jazzHands.gif' alt='jazzhands'>");
+
+        } else {
+            card.append("<h3>You Made Gus Sad With Your Lack Of Knowledge</h3>");
+            card.append("<br><img src='assets/images/gus_sad.gif' alt='jazzhands'>");
+        }
+
+
+
+
     },
 
-    clicked: function(e) {
+    clicked: function (e) {
         clearInterval(timer);
-        if ($(e.target).attr("data-name") === myQuestions[this.currentQuestion].correct){
+        if ($(e.target).attr("data-name") === myQuestions[this.currentQuestion].correct) {
             this.isCorrect();
         }
-        else{
+        else {
             this.isWrong();
         }
     },
 
-    isCorrect : function() {
+    isCorrect: function () {
         clearInterval(timer);
-        game.correct++ ;
+        game.correct++;
         card.html("<h2> Correct!</h2>");
         card.append("<img src='" + myQuestions[game.currentQuestion].images + "'/>");
-        if (game.currentQuestion === myQuestions.length -1 ) {
-            setTimeout(game.results, 3 * 1000); 
-        }
-        else {
-            setTimeout(game.nextQuestion, 3 * 1000);
-        }
-    },
-    isWrong : function() {
-        game.incorrect++ ; 
-        clearInterval(timer);
-        card.html("<h2> Wrong!</h2>");
-        card.append("<h3> Correct Answer was : " + myQuestions[game.currentQuestion].correct + "</h3>");
-        card.append("<img src ='" + myQuestions[game.currentQuestion].images + "'/>");
-        if (game.currentQuestion === myQuestions.length -1 ) {
+        if (game.currentQuestion === myQuestions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         }
         else {
             setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
-    reset: function() {
+    isWrong: function () {
+        game.incorrect++;
+        clearInterval(timer);
+        card.html("<h2> Wrong!</h2>");
+        card.append("<h3> Correct Answer was : " + myQuestions[game.currentQuestion].correct + "</h3>");
+        card.append("<img src ='" + myQuestions[game.currentQuestion].images + "'/>");
+        if (game.currentQuestion === myQuestions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    reset: function () {
         this.currentQuestion = 0;
         this.counter = 30;
         this.correct = 0;
         this.incorrect = 0;
         this.loadQuestion();
-      }
+    }
 };
-$(document).on("click", "#start-over", function() {
+$(document).on("click", "#start-over", function () {
     game.reset();
-  });
-  
-  $(document).on("click", "#answer-button", function(e) {
+});
+
+$(document).on("click", "#answer-button", function (e) {
     game.clicked(e);
-  });
-  
-  $(document).on("click", "#start-button", function() {
+});
+
+$(document).on("click", "#start-button", function () {
     console.log(game.counter);
     $('#time-remaining').text(game.counter);
     game.loadQuestion();
-  });
+});
 
 
 
